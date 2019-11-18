c72aebb4f7f51d9a7ee81c8513860e071f3a2217
Soraya Estalayo <sestalayos@gmail.com>
2019-11-18 20:32:10 +0100

    Creando el juego del ahorcado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,54 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ahorcado';
+  palabra = 'AGUACATE';
+  palabraOculta = '';
+
+  letras = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'Ñ',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+
+  constructor() {
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
+  comprobar(letra) {
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+  }
 }
