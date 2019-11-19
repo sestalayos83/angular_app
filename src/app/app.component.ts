6d84ae675dd22c89e7c72cc41a62e9367b16c24c
Soraya Estalayo <sestalayos@gmail.com>
2019-11-19 20:26:12 +0100

    Verificar gana o pierde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,54 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  palabra = 'AGUACATE';
-  palabraOculta = '';
+  palabra = "AGUACATE";
+  palabraOculta = "";
 
   intentos = 0;
 
+  gano = false;
+  perdio = false;
+
   letras = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'Ñ',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "Ñ",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
   ];
 
   constructor() {
-    this.palabraOculta = '_ '.repeat(this.palabra.length);
+    this.palabraOculta = "_ ".repeat(this.palabra.length);
   }
   comprobar(letra) {
     this.existeLetra(letra);
-    const palabraOcultaArr = this.palabraOculta.split(' ');
+    const palabraOcultaArr = this.palabraOculta.split(" ");
 
     for (let i = 0; i < this.palabra.length; i++) {
       if (this.palabra[i] === letra) {
@@ -53,14 +56,28 @@ export class AppComponent {
       }
     }
 
-    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.palabraOculta = palabraOcultaArr.join(" ");
+    this.verificaGane();
+  }
+
+  verificaGane() {
+    const palabraArr = this.palabraOculta.split(" ");
+    const palabraEvaluar = palabraArr.join("");
+
+    if (palabraEvaluar === this.palabra) {
+      this.gano = true;
+      console.log("el usuario ganó");
+    }
+
+    if (this.intentos >= 9) {
+      this.perdio = true;
+      console.log("el usuario perdio");
+    }
   }
 
   existeLetra(letra) {
     if (this.palabra.indexOf(letra) >= 0) {
-      console.log('La letra existe: ' + letra);
     } else {
-      console.log('La letra No existe: ' + letra);
       this.intentos++;
     }
   }
